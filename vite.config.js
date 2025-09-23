9cdd071c226a5c9f276697b97414c0c1d4102da4
bipin-Pariyar2003 <163013814+bipin-Pariyar2003@users.noreply.github.com>
2025-09-23 18:53:09 +0545

    pwa support

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,35 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/", // important for Vercel SPA
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Bipin Pariyar Portfolio",
+        short_name: "BipinPortfolio",
+        description: "Portfolio of Bipin Pariyar",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
